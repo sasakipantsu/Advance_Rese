62681ce3ac8810d53e2ef0c51c1315de059175bb
pantsu <sasakissan@gmail.com>
2022-01-07 15:27:27 +0900

    マイページのお気に入り店舗表示ok

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -3,24 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Shop;
-use App\Models\Favorite;
 use Illuminate\Support\Facades\Auth;
 
 class MypageController extends Controller
 {
     public function mypage()
     {
-        // $shops = Shop::with('genres')->get();
-
-        $shops = Favorite::where('user_id', \Auth::user()->id)->get()->pluck('shop_id');
-
-        // $shops = Shop::where('user_id', \Auth::user()->id)
-        //                     ->whereIn('to_user_id', $likedUserIds)
-        //                     ->where('is_like', true)
-        //                     // N+1問題回避のためにcontrollerにこの記述をする
-        //                     ->with('toUser')
-        //                     ->get();
+        $shops = Auth::user()->shops;
 
         return view('my_page', ['shops' => $shops]);
     }
